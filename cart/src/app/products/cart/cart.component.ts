7b24b02b3d682d0556ee153ec1cb1d71f54dd586
NIJIN JOY <nijinjoy1999@gmail.com>
2023-02-04 19:32:38 +0530

    ecommercefinal

--- cart/src/app/products/cart/cart.component.ts
+++ cart/src/app/products/cart/cart.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../cart.service';
-
+import { Router } from '@angular/router';
+import party from "party-js";
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
@@ -9,9 +10,10 @@ import { CartService } from '../cart.service';
 export class CartComponent implements OnInit {
 
   cartitems:any=[]
-  grand:Number=0;
+  grand:any
+  updatetotal:any
 
-  constructor(private cart:CartService) { }
+  constructor(private cart:CartService,private router:Router) { }
 
   ngOnInit(): void {
     this.cart.cartlist.subscribe(
@@ -29,6 +31,29 @@ export class CartComponent implements OnInit {
   }
   removeall(){
     this.cart.removeall()
+  }
+  discount3per(source:any){
+    party.confetti(source)
+  let discount=(this.grand*3)/100
+  this.updatetotal=this.grand-discount
+
+  }
+  discount10per(){
+   let discount=(this.grand*10)/100
+   this.updatetotal=this.grand-discount
+  }
+  discount30per(){
+    let discount=(this.grand*30)/100
+    this.updatetotal=this.grand-discount
+  }
+  discount50per(){
+    let discount=(this.grand*50)/100
+    this.updatetotal=this.grand-discount
+  }
+  proceed(){
+    alert('Your order is placed')
+    this.router.navigateByUrl('')
+    this.removeall()
   }
 
 }
